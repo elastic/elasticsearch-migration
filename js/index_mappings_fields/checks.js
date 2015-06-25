048155f6760c922279e325d21b687f1ae92469c5
Clinton Gormley <clint@traveljury.com>
2015-06-25 20:11:06 +0200

    Added checks for compressed binary fields and deprecated fielddata formats

--- js/index_mappings_fields/checks.js
+++ js/index_mappings_fields/checks.js
@@ -1,33 +1,55 @@
 "use strict";
 
-Checks.register("index.mappings.fields", [
+Checks
+  .register(
+    "index.mappings.fields",
+    [
 
-  {
-    name : "Use of `index_name` or `path`",
-    color : "blue",
-    msg : "The `path` and `index_name` parameters are deprecated",
-    check : function(field) {
-      return field.path || field.index_name;
-    }
-  },
+      {
+        name : "Use of `index_name` or `path`",
+        color : "blue",
+        msg : "The `path` and `index_name` parameters are deprecated",
+        check : function(field) {
+          return field.path || field.index_name;
+        }
+      },
 
-  {
-    name : "Boolean fields",
-    color : "blue",
-    msg : "Boolean fields will return `1/0` instead of `T/F` in scripts, "
-      + "aggregations, or sort values",
-    check : function(field) {
-      return field.type && field.type === "boolean";
-    }
-  },
+      {
+        name : "Boolean fields",
+        color : "blue",
+        msg : "Boolean fields will return `1/0` instead of `T/F` in scripts, "
+          + "aggregations, or sort values",
+        check : function(field) {
+          return field.type && field.type === "boolean";
+        }
+      },
 
-  {
-    name : "Per-field postings format",
-    color : "red",
-    msg : "The `postings_format` parameter is no longer supported",
-    check : function(field) {
-      return field.postings_format;
-    }
-  },
+      {
+        name : "Per-field postings format",
+        color : "red",
+        msg : "The `postings_format` parameter is no longer supported",
+        check : function(field) {
+          return field.postings_format;
+        }
+      },
 
-]);
+      {
+        name : "Binary field compression",
+        color : "blue",
+        msg : "The `compress` and `compress_threshold` parameters are no longer supported",
+        check : function(field) {
+          return field.hasOwnProperty('compress') || field.compress_threshold;
+        }
+      },
+
+      {
+        name : "Fielddata formats",
+        color : "blue",
+        msg : "The `fst` and `compressed` fielddata formats are no longer supported and will be ignored",
+        check : function(field) {
+          var format = Checks.get_key(field, 'fielddata.format');
+          return format === 'fst' || format === 'compressed';
+        }
+      }
+
+    ]);
